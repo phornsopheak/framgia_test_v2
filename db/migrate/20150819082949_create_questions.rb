24bf408137d8a8c958e443b2eaae8c92d899d68c
Lim Kimhuor <kimhuorlim@gmail.com>
2015-08-19 15:38:06 +0700

    Init model

---
+++ db/migrate/20150819082949_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.text :content
+      t.string :question_type
+      t.boolean :state
+      t.references :subject, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,102 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150819083145) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content",     limit: 65535
+    t.boolean  "correct"
+    t.integer  "question_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+
+  create_table "exams", force: :cascade do |t|
+    t.integer  "score",      limit: 4
+    t.string   "status",     limit: 255
+    t.integer  "time",       limit: 4
+    t.integer  "user_id",    limit: 4
+    t.integer  "subject_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "exams", ["subject_id"], name: "index_exams_on_subject_id", using: :btree
+  add_index "exams", ["user_id"], name: "index_exams_on_user_id", using: :btree
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content",       limit: 65535
+    t.string   "question_type", limit: 255
+    t.boolean  "state"
+    t.integer  "subject_id",    limit: 4
+    t.integer  "user_id",       limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "questions", ["subject_id"], name: "index_questions_on_subject_id", using: :btree
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
+
+  create_table "results", force: :cascade do |t|
+    t.text     "content",     limit: 65535
+    t.integer  "question_id", limit: 4
+    t.integer  "exam_id",     limit: 4
+    t.integer  "answer_id",   limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "results", ["answer_id"], name: "index_results_on_answer_id", using: :btree
+  add_index "results", ["exam_id"], name: "index_results_on_exam_id", using: :btree
+  add_index "results", ["question_id"], name: "index_results_on_question_id", using: :btree
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.integer  "number_of_question", limit: 4
+    t.integer  "duration",           limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                   limit: 255
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "avatar",                 limit: 255
+    t.boolean  "admin",                              default: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "exams", "subjects"
+  add_foreign_key "exams", "users"
+  add_foreign_key "questions", "subjects"
+  add_foreign_key "questions", "users"
+  add_foreign_key "results", "answers"
+  add_foreign_key "results", "exams"
+  add_foreign_key "results", "questions"
+end
