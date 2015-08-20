2fb3b0fb26683b7d195bc2a90e097d6350922ed6
Phorn Sopheak <phornsopheak17@gmail.com>
2015-08-20 12:52:09 +0700

    show_subject_list

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users
   root "staticpages#home"
+
+  resources :subjects, only: [:index]
 end
